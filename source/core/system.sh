fbc6e8493ce51c643d6b42bc7203bb8e40f70522
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-10-08 15:17:09 -0700

    Portable mktemp and base64 decode
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

---
+++ source/core/system.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+##########################################################################
+# For internal use only
+# ------
+# This is meant to provide portable code for system operation calling on
+# binaries which implementations vary too significantly.
+##########################################################################
+
+portable::mktemp(){
+  mktemp -q "${TMPDIR:-/tmp}/$1.XXXXXX" 2>/dev/null || mktemp -q
+}
+
+portable::base64Decode(){
+  local os="$(uname)"
+
+  case "$os" in
+    Darwin)
+      base64 -D
+    ;;
+    *)
+      base64 -d
+    ;;
+  esac
+}
