2993b9719270dcf5203ddf72ce6d0abea8415bcb
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-10-09 00:44:43 -0700

    Better test grouping for HTTP and IMDB
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- tests/integration/http/test.sh
+++ tests/integration/http/test.sh
@@ -3,31 +3,39 @@
 
 #python -m SimpleHTTPServer 12345
 
-testHTTP(){
+testHTTPDNSResolutionFailure(){
   # DNS resolution failure
   result=$(dc-http -s https://WHATEVERTHISISITWILLFAIL  "HEAD")
   exit=$?
   dc-tools::assert::equal "http dns failure exit code" "$exit" "$ERROR_NETWORK"
   dc-tools::assert::equal "$result" ""
+}
 
+testHTTPNoServer(){
   # No response at that address
   result=$(dc-http -s https://locahost:12345 "HEAD")
   exit=$?
   dc-tools::assert::equal "$exit" "$ERROR_NETWORK"
   dc-tools::assert::equal "$result" ""
+}
 
+testHTTPUnexpectedTLS(){
   # HTTP on HTTPS
   result=$(dc-http -s https://www.google.com:80 "HEAD")
   exit=$?
   dc-tools::assert::equal "$exit" "$ERROR_NETWORK"
   dc-tools::assert::equal "$result" ""
+}
 
+testHTTPUnexpectedNoTLS(){
   # HTTPS on HTTP
   result=$(dc-http -s http://www.google.com:443 "HEAD")
   exit=$?
   dc-tools::assert::equal "$exit" "$ERROR_NETWORK"
   dc-tools::assert::equal "$result" ""
+}
 
+testHTTPRedirect(){
   # Redirect
   result=$(dc-http -s https://google.com "HEAD")
   exit=$?
@@ -41,7 +49,9 @@ testHTTP(){
   dc-tools::assert::equal "$redirected" "https://www.google.com/"
   dc-tools::assert::equal "$body" ""
   dc-tools::assert::equal "$server" "gws"
+}
 
+testHTTPValidHEAD(){
   # Valid HEAD request
   result=$(dc-http -s https://www.google.com "HEAD")
   exit=$?
@@ -53,7 +63,9 @@ testHTTP(){
   dc-tools::assert::equal "$status" "200"
   dc-tools::assert::equal "$redirected" ""
   dc-tools::assert::equal "$body" ""
+}
 
+testHTTPValidGET(){
   # Valid GET request
   result=$(dc-http -s https://registry-1.docker.io/v2 "GET")
   exit=$?
@@ -65,5 +77,4 @@ testHTTP(){
   dc-tools::assert::equal "$status" "401"
   dc-tools::assert::equal "$redirected" "/v2/"
   dc-tools::assert::equal "$body" '{"errors":[{"code":"UNAUTHORIZED","message":"authentication required","detail":null}]}'
-
 }
--- tests/integration/imdb/test.sh
+++ tests/integration/imdb/test.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 
-testIMDB(){
+testIMDBBogusID(){
   result=$(dc-imdb -s "BOGUS")
   exit=$?
   dc-tools::assert::equal "bogus ID" "$exit" "$ERROR_ARGUMENT_INVALID"
   dc-tools::assert::equal "$result" ""
+}
 
+testIMDBNonExistentID(){
   result=$(dc-imdb -s "tt0000000")
   exit=$?
   dc-tools::assert::equal "bogus ID" "$exit" "$ERROR_NETWORK"
   dc-tools::assert::equal "$result" ""
+}
 
+testIMDBData(){
   result=$(dc-imdb -s "tt0000001" | jq -rc .)
   exit=$?
   dc-tools::assert::equal "$exit" "0"
   dc-tools::assert::equal "$result" '{"title":"Carmencita","original":"Carmencita","picture":"https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_.jpg","year":"1894","type":"movie","runtime":["1 min"],"ratio":"1.33 : 1","id":"tt0000001","properties":{"SOUND_MIX":"Silent","COLOR":"Black and White","FILM_LENGTH":"15.24 m","NEGATIVE_FORMAT":"35 mm (Eastman)","CINEMATOGRAPHIC_PROCESS":"Kinetoscope","PRINTED_FILM_FORMAT":"35 mm"}}'
+}
 
+testIMDBImage(){
   filename="$(portable::mktemp dc::http::request)"
   dc-imdb -s --image=dump "tt0000001" > "$filename"
   exit=$?
