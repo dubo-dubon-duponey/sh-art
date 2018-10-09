01b5c1ca1caf4116cd9a7f8b37ab5d6fd608dcde
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-10-08 22:43:29 -0700

    Embark shunit2 for testing
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- tests/unit/string.uplow.sh
+++ tests/unit/string.uplow.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-source="∞Foo"
-result=$(dc::string::toUpper source)
-dc-tools::assert::equal "$result" "∞FOO" "upper"
-result=$(dc::string::toLower source)
-dc-tools::assert::equal "$result" "∞foo" "lower"
+testStringUpLow(){
+  source="∞Foo"
+  result=$(dc::string::toUpper source)
+  dc-tools::assert::equal "$source to upper" "$result" "∞FOO"
+  result=$(dc::string::toLower source)
+  dc-tools::assert::equal "$source to lower" "$result" "∞foo"
 
-source=""
-result=$(dc::string::toUpper source)
-dc-tools::assert::equal "$result" "" "upper"
-result=$(dc::string::toLower source)
-dc-tools::assert::equal "$result" "" "lower"
+  source=""
+  result=$(dc::string::toUpper source)
+  dc-tools::assert::equal "$source to upper" "$result" ""
+  result=$(dc::string::toLower source)
+  dc-tools::assert::equal "$source to lower" "$result" ""
 
-source="∞Foo"$'\n'"bar"
-result=$(printf "%s" "$source" | dc::string::toUpper)
-dc-tools::assert::equal "$result" "∞FOO"$'\n'"BAR" "upper"
-result=$(printf "%s" "$source" | dc::string::toLower)
-dc-tools::assert::equal "$result" "∞foo"$'\n'"bar" "lower"
+  source="∞Foo"$'\n'"bar"
+  result=$(printf "%s" "$source" | dc::string::toUpper)
+  dc-tools::assert::equal "$source to upper" "$result" "∞FOO"$'\n'"BAR"
+  result=$(printf "%s" "$source" | dc::string::toLower)
+  dc-tools::assert::equal "$source to lower" "$result" "∞foo"$'\n'"bar"
+}
+
