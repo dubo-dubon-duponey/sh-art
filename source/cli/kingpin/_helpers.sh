4d61febb10b71ac06f9d5ba6e6337f469ca28539
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-10-09 17:19:46 -0700

    Defensive against missing bento + libav workaround for travis
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- source/cli/kingpin/_helpers.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-helpers::profile_link(){
-  local posh="$1"
-  if ! grep -q "$posh" "$HOME/.profile"; then
-    printf "%s\\n" "# shellcheck source=$HOME/$posh" >> "$HOME/.profile"
-    printf "%s\\n" ". \"\$HOME/$posh\"" >> "$HOME/.profile"
-  fi
-}
