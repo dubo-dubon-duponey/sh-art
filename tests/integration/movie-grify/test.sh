2738193c07444fafb46aed94f0fd674378735823
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-10-14 21:45:55 -0700

    Fix converter
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- tests/integration/movie-grify/test.sh
+++ tests/integration/movie-grify/test.sh
@@ -6,6 +6,9 @@ testMovieGrify(){
   local result
 
   [ ! -f "tests/integration/movie-grify/movie.mp4" ] || rm "tests/integration/movie-grify/movie.mp4"
+  ffmpeg -i tests/integration/movie-grify/movie.avi -movflags faststart -map 0 -c copy -map -0:1 -map 0:1 -c:a:0 libfdk_aac -b:a 256k tests/integration/movie-grify/movie.mp4
+
+  [ ! -f "tests/integration/movie-grify/movie.mp4" ] || rm "tests/integration/movie-grify/movie.mp4"
   result=$(DC_MOVIE_GRIFY_LOG_LEVEL=debug dc-movie-grify --destination=tests/integration/movie-grify --convert=1 --remove=1 tests/integration/movie-grify/movie.avi)
   local exit=$?
   dc-tools::assert::equal "$exit" "0"
