0ada519a9c6b46494962a0a274ff34e94f3fcab9
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-10-24 14:58:57 -0700

    Cli / commander cleanup
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- source/cli/movie-info/cmd.sh
+++ source/cli/movie-info/cmd.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-readonly CLI_VERSION="1.0.0-rc"
+readonly CLI_VERSION="0.1.0"
 readonly CLI_LICENSE="MIT License"
 readonly CLI_DESC="spits out information about media files in a json format (duration, container, size, and for each track, codec, resolution or language)"
-readonly CLI_USAGE="[-s] filename"
 
 # Initialize
 dc::commander::initialize
-
+dc::commander::declare::arg 1 ".+" "" "filename" "media file to be analyzed"
 # Start commander
 dc::commander::boot
 # Requirements
