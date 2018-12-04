e1b27faf60cfeb5704e31100a109534803d0cc14
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-12-03 16:12:26 -0800

    Make lint happy
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- tests/unit/arg.sh
+++ tests/unit/arg.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 testArgGrep() {
+  # shellcheck disable=SC2034
   DC_PARGV_1=foobar
+  # shellcheck disable=SC2034
   DC_PARGE_1=true
   local result
+  # shellcheck disable=SC2034
   result="$(dc::commander::declare::arg 1 "^foo(?:bar)?$" "" "a flag" "a flag")"
   local exit=$?
   dc-tools::assert::equal "$exit" "0"
 
+  # shellcheck disable=SC2034
   result="$(dc::commander::declare::arg 1 "^foo(?:abar)?$" "" "another flag" "another flag")"
   local exit=$?
   dc-tools::assert::equal "$exit" "$ERROR_ARGUMENT_INVALID"
