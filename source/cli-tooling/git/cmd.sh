958c91dc2abb1c1a7f59a620b4dc45ac9be9ec31
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2018-12-06 14:50:51 -0800

    Enhancements to the git helper
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- source/cli-tooling/git/cmd.sh
+++ source/cli-tooling/git/cmd.sh
@@ -10,29 +10,37 @@ dc::commander::boot
 # Requirements
 dc::require git
 
-allcommits="$(git log --format=%H -C "$1")"
+allcommits="$(git -C "$1" log --format=%H)"
 regex="^Signed-off-by: ([^<]+) <([^<>@]+@[^<>]+)>( \\(github: ([a-zA-Z0-9][a-zA-Z0-9-]+)\\))?$"
 
 for commit in ${allcommits}; do
   dc::logger::debug "Analyzing $commit"
-  if [ -z "$(git log -1 --format='format:' --name-status "$commit")" ]; then
+  if [ -z "$(git -C "$1" log -1 --format='format:' --name-status "$commit")" ]; then
     # no content (ie, Merge commit, etc)
     dc::logger::warning "Ignoring commit $commit"
     continue
   fi
-  if ! git log -1 --format='format:%B' "$commit" | grep -qE "$regex"; then
+  if ! git -C "$1" log -1 --format='format:%B' "$commit" | grep -qE "$regex"; then
     badCommits+=( "$commit" )
-    dc::logger::error "NOT signed appropriately"
+    dc::logger::error "NOT signed-off appropriately"
   else
-    dc::logger::debug "Commit is signed appropriately"
+    dc::logger::debug "Commit is signed-off appropriately"
+  fi
+  if ! git -C "$1" verify-commit "$commit" 2>/dev/null; then
+    badCommits+=( "$commit" )
+    dc::logger::error "NOT gpg signed properly"
   fi
 done
 
 if ! [ ${#badCommits[@]} -eq 0 ]; then
-  dc::logger::error "These commits are not signed properly:"
+  dc::logger::error "These commits are problematic:"
   for commit in "${badCommits[@]}"; do
     dc::logger::error " - $commit"
   done
-  dc::logger::error 'Please amend each commit to sign them.'
+  dc::logger::error 'Please amend.'
   exit "$ERROR_FAILED"
 fi
+
+
+# Resign everything with gpg
+# git filter-branch --commit-filter 'git commit-tree -S "$@";' -- --all
