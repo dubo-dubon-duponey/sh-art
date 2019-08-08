16f94c6c1ad7e399aa47f51e8bb01aa710ce5722
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-08-07 23:33:53 -0700

    One more fix and cleanup
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- dckr.test-all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for i in ubuntu-lts-old ubuntu-lts-current ubuntu-current ubuntu-next alpine-current alpine-next debian-old debian-current debian-next; do
-  if ! DOCKERFILE=./dckr.Dockerfile TARGET="$i" dckr make test; then
-    echo "FAILED"
-    exit 1
-  fi
-done
