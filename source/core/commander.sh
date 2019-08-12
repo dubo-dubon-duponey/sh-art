aede9c20bb0a8e66f86f10fc3b6c337077951bed
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-08-11 23:40:11 -0700

    Removing inaccurate text
    2>/dev/null would also swallow-up read prompts messages
    
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- source/core/commander.sh
+++ source/core/commander.sh
@@ -189,7 +189,7 @@ dc::commander::declare::flag(){
 
 # shellcheck disable=SC2120
 dc::commander::initialize(){
-  dc::commander::declare::flag "silent" "^$" "no logging (overrides log level) - equivalent to: ${CLI_NAME:-${DC_CLI_NAME}} 2>/dev/null" optional "s"
+  dc::commander::declare::flag "silent" "^$" "no logging (overrides log level)" optional "s"
   dc::commander::declare::flag "insecure" "^$" "disable TLS verification for network operations (CAREFUL)" optional
 
   local loglevelvar
