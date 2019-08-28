920899253322c3d4dbe0025e387e0675abc04110
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-08-28 16:59:05 -0700

    + signal handling tests
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- tests/integration/libre/max.sh
+++ tests/integration/libre/max.sh
@@ -19,3 +19,60 @@ testLibre(){
 #dc-tools::assert::null ARGV_TEST5
 #dc-tools::assert::null ARGV_TEST6
 #dc-tools::assert::equal ARGV_TEST7 '""'
+
+commandWrapper(){
+  local pid
+
+  "$@" 1>/dev/null 2>&1 &
+  pid=$!
+  printf "%s\n" "$pid"
+  wait "$pid"
+  printf "%s\n" "$?"
+}
+
+testSignals(){
+  local pid
+  local ex
+  local signal
+  local x=127
+  # SIGINT and SIGQUIT are not testable in that context https://unix.stackexchange.com/questions/356408/strange-problem-with-trap-and-sigint
+  for signal in "" "SIGHUP" "" "" "" "" "SIGABRT" "" "" "SIGKILL" "" "" "" "" "SIGALRM" "SIGTERM"; do
+    x=$(( x + 1 ))
+    if [ ! "$signal" ]; then
+      continue
+    fi
+    pid=
+    ex=
+    while read -r line; do
+      if [ ! "$pid" ]; then
+        pid="$line"
+        kill -s "$signal" "$pid"
+        continue
+      fi
+      ex="$line"
+    done < <(commandWrapper "./bin/bootstrap" "--help" "--$signal" 2>/dev/null)
+    dc-tools::assert::equal "signal handling $signal" "$ex" "$x"
+  done
+}
+
+# XXX Need to instrument dc-libre for that when this gets moved to sh-art
+# 1 - caught by ERR
+# let "var1 = 1/0"
+# ls -lA /goo
+
+# 2 - NOT caught by ERR
+# empty_function() {}
+
+# 126 - caught by err
+# /dev/null
+
+# 127 - caught by err
+# thisfails
+
+# 128
+# XXX not sure how to do this
+# exit "3.14"
+
+# 255
+# apparently, bash does $ex % 255 so out of range never triggers...
+# exit 257
