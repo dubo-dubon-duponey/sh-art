7c084282ad0c893e539704d00e7755f0c62f694f
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2020-04-03 17:10:01 -0700

    Linter
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- tests/unit/1a.sh
+++ tests/unit/1a.sh
@@ -3,22 +3,21 @@
 testBasicValidationNoBash(){
   local result
 
-  result="$(sh source/core/1a-bash.sh 2>&1)" && {
+  if result="$(sh source/core/1a-bash.sh 2>&1)"; then
     dc-tools::assert::equal "should never succeed" true false
-  } || {
-    dc-tools::assert::equal "exit code" 144 "$?"
-  }
+  fi
+  dc-tools::assert::equal "exit code" 144 "$?"
 
   dc-tools::assert::contains "response was" "$result" "This only works with bash."
 }
 
 testBasicValidationNoPSNoBash(){
   local result
-  result="$(PATH=/nonexistent; /bin/sh source/core/1a-bash.sh 2>&1)" && {
+  # shellcheck disable=SC2123
+  if result="$(PATH=/nonexistent; /bin/sh source/core/1a-bash.sh 2>&1)"; then
     dc-tools::assert::equal "should never succeed" true false
-  } || {
-    dc-tools::assert::equal "exit code" 144 "$?"
-  }
+  fi
+  dc-tools::assert::equal "exit code" 144 "$?"
 
   dc-tools::assert::contains "response contained lacks ps" "$result" "Your system lacks ps"
   dc-tools::assert::contains "response contained no bash" "$result" "This only works with bash (BASH: /bin/sh"
@@ -27,6 +26,7 @@ testBasicValidationNoPSNoBash(){
 testBasicValidationNoPS(){
   local result
 
+  # shellcheck disable=SC2123,SC2030
   result="$(PATH=/nonexistent; /bin/bash source/core/1a-bash.sh 2>&1)" || {
     dc-tools::assert::equal "should not fail" true false
   }
@@ -38,32 +38,33 @@ testBasicValidationNoPS(){
 
 testBasicValidationBrokenPS(){
   local result
-  echo 'if [ "$*" == "-o ppid,comm" ]; then /bin/ps $*; else exit 1; fi' > ps
+  echo 'if [ "$*" == "-o ppid,comm" ]; then /bin/ps $*; else exit 1; fi' > "ps"
   chmod u+x ps
 
+  # shellcheck disable=SC2030,SC2031
   result="$(PATH="$(pwd):$PATH"; /bin/bash source/core/1a-bash.sh 2>&1)" || {
     dc-tools::assert::equal "should not fail" true false
   }
 
   dc-tools::assert::contains "response contained broken ps" "$result" "Your ps does not support -p (busybox?)"
 
-  rm ps
+  rm "ps"
 }
 
 testBasicValidationBrokenPSNoBash(){
   local result
-  echo 'if [ "$*" == "-o ppid,comm" ]; then /bin/ps $*; else exit 1; fi' > ps
+  echo 'if [ "$*" == "-o ppid,comm" ]; then /bin/ps $*; else exit 1; fi' > "ps"
   chmod u+x ps
 
-  result="$(PATH="$(pwd):$PATH"; /bin/sh source/core/1a-bash.sh 2>&1)" && {
+  # shellcheck disable=SC2030,SC2031
+  if result="$(PATH="$(pwd):$PATH"; /bin/sh source/core/1a-bash.sh 2>&1)"; then
     dc-tools::assert::equal "should never succeed" true false
-  } || {
-    dc-tools::assert::equal "exit code" 144 "$?"
-  }
+  fi
+  dc-tools::assert::equal "exit code" 144 "$?"
 
   dc-tools::assert::contains "response contained broken ps" "$result" "Your ps does not support -p (busybox?)"
   dc-tools::assert::contains "response was" "$result" "This only works with bash."
 
-  rm ps
+  rm "ps"
 }
 
